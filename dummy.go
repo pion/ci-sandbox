34e55215125c7ad907f5088a784fb628b9203cc2
Daniele Sluijters <daenney@users.noreply.github.com>
2022-05-24 10:13:45 +0200

    Trigger CodeQL on a PR

--- dummy.go
+++ dummy.go
@@ -3,9 +3,16 @@ package sandbox
 
 import (
 	"errors"
+	"strconv"
 
 	_ "github.com/pion/transport/test" // nolint
 )
 
 // ErrDummy is a dummy.
 var ErrDummy = errors.New("dummy")
+
+func codeQL() {
+	res, _ := strconv.Atoi("10")
+	num := uint8(res)
+	_ = num
+}
